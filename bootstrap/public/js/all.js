026b449d3c75f56911353b1b60c1d01f615adf4c
indisc <indisc@posteo.de>
2016-03-03 21:57:00 +0100

    picture gallery

--- bootstrap/public/js/all.js
+++ bootstrap/public/js/all.js
@@ -1,2 +1,2 @@
-$(document).ready(function(){$(".navbar-toggle").on("click",function(){$(this).toggleClass("active")})});
+$(document).ready(function(){$(".navbar-toggle").on("click",function(){$(this).toggleClass("active")})}),$(document).ready(function(){$("picture img").click(function(){var t=$(this).attr("src"),e=$(this).attr("h1"),i=$(this).attr("p");$("#bigPic").attr({src:t,h1:e,p:i})})});
 //# sourceMappingURL=all.js.map
